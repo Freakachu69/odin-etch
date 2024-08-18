84482a11b5056977af011a643411c896556f1d34
Freakachu69 <151987985+Freakachu69@users.noreply.github.com>
2024-08-17 23:47:13 -0400

    Add button to create custom grid The prompt does not accept values that aren't a number, or any numbers less than zero or greater than 100. The prompt loops if invalid inputs are repeatedly entered.

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 let container = document.querySelector(".container");
-for (let i=0; i<256; i++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    //box.addEventListener('mouseover', ()=> {
-    //   box.style.backgroundColor = "black";
-    //});
-    container.appendChild(box);
+let btn = document.querySelector(".btn");
+
+function etch(squares) {
+    let area = squares * squares;
+    let basis = (1/squares) * 100;
+    for (let i = 0; i < area; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.style.flexBasis = `${basis}%`;
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = "black";
+        });
+        container.appendChild(box);
+    }
 }
-let boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', ()=> {
-        box.style.backgroundColor = "black";
-    });
+
+btn.addEventListener('click', ()=> {
+    let input = prompt("How many squares per side? (Max:100)");
+    while (input > 100 || input < 1) {
+        input = prompt("Please choose a number between 1 and 100. Try again.");
+    }
+    while (isNaN(input)) {
+        input = prompt("Please choose a number. Try again.")
+        while (input > 100 || input < 1) {
+            input = prompt("Please choose a number between 1 and 100. Try again.");
+        }
+    }
+    container.replaceChildren();
+    etch(input);
 });
+
+etch(16);
